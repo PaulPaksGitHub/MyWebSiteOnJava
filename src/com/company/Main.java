aa3d2b343cc78cf6409792a6c2981239c63f813b
PaulPaks <pakseev.1999@gmail.com>
2018-03-18 21:15:53 +0300

    User was added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello world!");
+        User s = new User("oaha", "1234");
+	    System.out.println(s.login);
     }
 }
---
+++ src/com/company/User.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class User {
+    String login;
+    String pass;
+    User(String log, String pas){
+        login = log;
+        pass = pas;
+    }
+}
