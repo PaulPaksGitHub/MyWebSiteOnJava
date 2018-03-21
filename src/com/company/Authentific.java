f63e59ffae6b2feaa4418bc2b4d0dd515cf8177b
PaulPaks <pakseev.1999@gmail.com>
2018-03-21 22:39:53 +0300

    Wrong System.out's deleted

--- src/com/company/Authentific.java
+++ src/com/company/Authentific.java
@@ -6,10 +6,8 @@ import java.util.List;
 public class Authentific {
     public static List<String> ROLES = Arrays.asList("READ", "WRITE", "EXECUTE");
     public static boolean auth(Parametrs param){
-        System.out.println("Authetification");
         if (ROLES.contains(param.role)){
             if (ResourceParser.authorizeFromAdress(param)){
-                System.out.println("Authetificated");
                 return true;
             } else {
                 System.exit(6);
--- src/com/company/Autorization.java
+++ src/com/company/Autorization.java
@@ -4,35 +4,28 @@ public class Autorization {
     public static boolean autorize(Parametrs param) {
 
         if (param.is_Empty()) {
-            System.out.println("Is empty");
             System.exit(6);
             return false;
         }
         else if (param.hasLogin() && param.hasPassword()) {
-            System.out.println("Autorization");
             User gettedUser = UserBase.getUserFromLogin(param);
             if (gettedUser.login.equals(param.login) && gettedUser.pass.equals(param.pass)){
-                System.out.println("Autorized");
                 return true;
             }
             else if (gettedUser.login != param.login){
-                System.out.println("Wrong login");
                 System.exit(1);
                 return false;
             }
             else if (gettedUser.pass != param.pass){
-                System.out.println("Wrong pass");
                 System.exit(2);
                 return false;
             }
         }
         else if (!param.hasLogin()){
-            System.out.println("Hasn't login");
             System.exit(1);
             return false;
         }
         else if (!param.hasPassword()){
-            System.out.println("Hasn't pass");
             System.exit(2);
             return false;
         }
