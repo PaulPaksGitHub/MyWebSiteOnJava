f00cbc390eee6ec13521890bcf67a03756de5906
PaulPaks <pakseev.1999@gmail.com>
2018-04-01 20:29:34 +0300

    Fix Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,35 @@ package com.company;
 import com.company.authentific.Authentific;
 import com.company.autorization.Autorization;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
         //STRING PARSER
-        //String comm = "-l pa -p 12 -r C.C.C -o READ";
-        String comm = args[0];
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader bistream = new BufferedReader(is);
+        String comm = null;
+        try {
+            comm = bistream.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         String[] line = comm.split(" ");
         Parametrs parametrs = ParseLine.parse(line);
         //GENERAL
-        if (parametrs.canAutorize()) {
+        if (parametrs.canAutorize() && parametrs.canAuthehtific() && parametrs.canAccaunt()) {
             Autorization.autorize(parametrs);
-            if (parametrs.canAuthehtific()) {
-                Authentific.auth(parametrs);
-                if (parametrs.canAccaunt()) {
-                    Accounting.account(parametrs);
-                }
-            }
+            Authentific.auth(parametrs);
+            Accounting.account(parametrs);
+        }
+        else if (parametrs.canAutorize() && parametrs.canAuthehtific()){
+            Autorization.autorize(parametrs);
+            Authentific.auth(parametrs);
+        }
+        else if (parametrs.canAutorize()){
+            Autorization.autorize(parametrs);
         }
     }
 }
