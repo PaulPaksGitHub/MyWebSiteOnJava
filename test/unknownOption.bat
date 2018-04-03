d5cb2ef2e44594ff6d41d58c825dd7ce482013d6
PaulPaks <pakseev.1999@gmail.com>
2018-04-03 22:00:06 +0300

    Test adapted

--- test/unknownOption.bat
+++ test/unknownOption.bat
@@ -1,5 +1,5 @@
 echo --egg hello | java -cp "..\out\production\prj;..\commons-cli-1.4.jar" com.company.Main 
 
 echo =====UnknownOption=====
-IF %ERRORLEVEL% EQU 6 ( echo OK )
-IF %ERRORLEVEL% NEQ 6 ( echo ### CRASHED ### )
+IF %ERRORLEVEL% EQU 0 ( echo OK )
+IF %ERRORLEVEL% NEQ 0 ( echo ### CRASHED ### )
