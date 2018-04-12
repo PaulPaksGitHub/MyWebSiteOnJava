bab68f928c2655140e6b4aedcd05316b9352bd73
PaulPaks <pakseev.1999@gmail.com>
2018-04-12 22:30:13 +0300

    Compil and create jar

--- BUILD.sh
+++ BUILD.sh
@@ -1,2 +1,2 @@
-javac -d bin -cp commons-cli-1.4.jar find -name "*.java"
-jar cvf project.jar -C bin .
+javac -sourcepath ./src -cp commons-cli-1.4.jar -d bin src/com/company/Main.java
+jar -cfm my.jar bin/META-INF/MANIFEST.MF -C bin .
