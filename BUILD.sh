5ef35ac1a5569167d3bb15ae6e4d653da935e6e1
PaulPaks <pakseev.1999@gmail.com>
2018-05-02 13:02:55 +0300

    fix BUILD

--- BUILD.sh
+++ BUILD.sh
@@ -1,7 +1,3 @@
 javac -sourcepath ./src -implicit:class -classpath "./libraries/*" -d bin src/com/company/Main.java
 
 jar -cvfm app.jar bin/META-INF/MANIFEST.MF -C bin .
-
-read -p "Please press enter to continue..." nothing
-
-
