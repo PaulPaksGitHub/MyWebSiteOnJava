3b0c50fa41d9682bf684140a7c706cb9ab26bb5c
PaulPaks <pakseev.1999@gmail.com>
2018-05-03 14:55:07 +0300

    Delete files after building

--- BUILD.sh
+++ BUILD.sh
@@ -1,3 +1,10 @@
+cd bin
+rm -r com
+cd ..
+
+rm app.jar
+rm app.log
+
 javac -sourcepath ./src -implicit:class -classpath "./libraries/*" -d bin src/com/company/Main.java
 
 jar -cvfm app.jar bin/META-INF/MANIFEST.MF -C bin .
