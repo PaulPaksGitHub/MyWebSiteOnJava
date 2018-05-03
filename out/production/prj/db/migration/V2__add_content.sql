9dce8632767720a4d6e4a150ff6f62bee0fc5c4d
PaulPaks <pakseev.1999@gmail.com>
2018-05-03 15:24:43 +0300

    Create table for accounting.

--- out/production/prj/db/migration/V2__add_content.sql
+++ out/production/prj/db/migration/V2__add_content.sql
@@ -1,14 +1,18 @@
-insert into users (id, login, pass, salt) values (1, 'pa', '1c597a01f767c2a9de609927a87946cc', '6B3BP7O4C8PJRMAA1VHAJD3YSEI0LITT');
-insert into users (id, login, pass, salt) values (2, 'ha', '7e9e025a501c75777682dd2405f4b231', 'ITSBQVZN8AJNL7E080XY6RX6S3PNSQ6X');
-insert into users (id, login, pass, salt) values (3, 'jdoe', 'a52833e3b81d7ffd3272f37dcb84fe20', '2K45ZH3G3WYK86RYDVGCG6ML4KPY2I9Y');
-insert into users (id, login, pass, salt) values (4, 'jrow', '1878356a24ead84df12e2afa97a4d0b9', '7X1PFTWGNZ7DO0RIINYQPWITMBPLLJ7J');
-insert into users (id, login, pass, salt) values (5, 'xxx', 'a051f8237087d0c4dd768505cf2952f9', 'VST7AR0FYAJ8KIK6QFQ6O58X82I9SRIU');
+insert into users ( login, pass, salt)
+values
+('pa', '1c597a01f767c2a9de609927a87946cc', '6B3BP7O4C8PJRMAA1VHAJD3YSEI0LITT'),
+('ha', '7e9e025a501c75777682dd2405f4b231', 'ITSBQVZN8AJNL7E080XY6RX6S3PNSQ6X'),
+('jdoe', 'a52833e3b81d7ffd3272f37dcb84fe20', '2K45ZH3G3WYK86RYDVGCG6ML4KPY2I9Y'),
+('jrow', '1878356a24ead84df12e2afa97a4d0b9', '7X1PFTWGNZ7DO0RIINYQPWITMBPLLJ7J'),
+('xxx', 'a051f8237087d0c4dd768505cf2952f9', 'VST7AR0FYAJ8KIK6QFQ6O58X82I9SRIU');
 
-insert into res (id, adress, login , id_user, role) values (1, 'A', 'pa', 1, 'READ');
-insert into res (id, adress, login , id_user, role) values (2, 'A', 'ha',2, 'EXECUTE');
-insert into res (id, adress, login , id_user, role) values (3, 'A.B', 'pa',1, 'EXECUTE');
-insert into res (id, adress, login , id_user, role) values (4, 'A.B.C', 'pa',1, 'WRITE');
-insert into res (id, adress, login , id_user, role) values (5 ,'a', 'jdoe',3, 'READ');
-insert into res (id, adress, login , id_user, role) values (6, 'a.b', 'jdoe',3, 'WRITE');
-insert into res (id, adress, login , id_user, role) values (7, 'a.b.c', 'jrow',4, 'EXECUTE');
-insert into res (id, adress, login , id_user, role) values (8 ,'a.bc', 'jdoe',3, 'EXECUTE');
+insert into res (adress, login , id_user, role)
+values
+('A', 'pa', 1, 'READ'),
+('A', 'ha',2, 'EXECUTE'),
+('A.B', 'pa',1, 'EXECUTE'),
+('A.B.C', 'pa',1, 'WRITE'),
+('a', 'jdoe',3, 'READ'),
+('a.b', 'jdoe',3, 'WRITE'),
+('a.b.c', 'jrow',4, 'EXECUTE'),
+('a.bc', 'jdoe',3, 'EXECUTE');
--- src/db/migration/V1__init_table.sql
+++ src/db/migration/V1__init_table.sql
@@ -1,17 +1,25 @@
 
 create table users
 (
-  id integer not null primary key ,
+  id integer not null auto_increment primary key ,
   login varchar(50) not null unique ,
   pass varchar(64) not null,
   salt varchar(64) not null
 );
 
 create table res (
-  id integer not null ,
+  id integer not null auto_increment,
   adress varchar(50) not null ,
   login varchar(50) not null references users(login),
   id_user integer not null references users(id) ,
   role varchar(20) not null,
   constraint res_pk primary key (login , role)
 );
+
+create table acc (
+  id integer not null auto_increment primary key ,
+  login varchar(50) not null references users(login),
+  ds varchar(15) not null ,
+  de varchar(15) not null ,
+  vol integer not null
+)
