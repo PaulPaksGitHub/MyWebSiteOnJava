aa75c1b3f97ac3000835a485adbe46939cdac311
PaulPaks <pakseev.1999@gmail.com>
2018-05-31 22:34:43 +0300

    Chech DATABASE_URL env on heroku

--- frontend/src/main/resources/db/migration/V1__init_table.sql
+++ frontend/src/main/resources/db/migration/V1__init_table.sql
@@ -1,23 +1,22 @@
 
 create table users
 (
-  id integer not null auto_increment primary key ,
+  id integer bigserial primary key ,
   login varchar(50) not null unique ,
   pass varchar(64) not null,
   salt varchar(64) not null
 );
 
 create table res (
-  id integer not null auto_increment,
+  id integer bigserial primary key,
   adress varchar(50) not null ,
   login varchar(50) not null references users(login),
   userid integer not null references users(id),
   role varchar(20) not null,
-  constraint res_pk primary key (login , role)
 );
 
 create table acc (
-  id integer not null auto_increment primary key ,
+  id integer bigserial primary key ,
   login varchar(50) not null references users(login),
   autorityid varchar(50) not null,
   ds varchar(15) not null ,
